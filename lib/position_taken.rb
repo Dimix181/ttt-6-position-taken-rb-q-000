c6682a8631ee9a59cf2a2416768891306d01c64b
Dimix181 <dimix181@gmail.com>
2016-04-07 09:08:18 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, position)
+  case 
+  when board[position.to_i]=="X"||board[position.to_i]=="O"
+    return true
+  else
+    return false
+    
+  end
+  
+end 
